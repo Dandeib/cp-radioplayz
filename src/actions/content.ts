e7775c23b4ee3115ea068acc6f5adef1df8f2bfc
dandeib <dev.dandeib@gmail.com>
2025-03-20 18:29:24 +0100

    fadsfds

--- src/actions/content.ts
+++ src/actions/content.ts
@@ -14,3 +14,36 @@ export async function createNews(content: string, image: string | null) {
     return true
 
 }
+
+export async function getAllNews() {
+    
+    const news = await db.news.findMany()
+
+    return news
+}
+
+export async function updateNews(id: string, content: string, image: string | null) {
+    
+    await db.news.update({
+        where: {
+            id
+        },
+        data: {
+            content,
+            image
+        }
+    })
+
+    return true
+}
+
+export async function deleteNews(id: string) {
+    
+    await db.news.delete({
+        where: {
+            id
+        }
+    })
+
+    return true
+}
--- src/app/dashboard/content/news/page.tsx
+++ src/app/dashboard/content/news/page.tsx
@@ -1,65 +1,128 @@
 'use client'
-import { createNews } from "@/actions/content";
+import { createNews, deleteNews, getAllNews, updateNews } from "@/actions/content";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { News } from "@prisma/client";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function NewsPage() {
-    const [content, setContent] = useState('');
-    const [image, setImage] = useState<File | null>(null);
-    const [base64Image, setBase64Image] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        
-        const handleSubmit = (e: React.FormEvent) => {
-            e.preventDefault();
-            
-            if (image) {
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    const base64String = reader.result as string;
-                    setBase64Image(base64String);
-                    createNews(content, base64String);
-                    toast.success('News erfolgreich erstellt');
-                };
-                reader.readAsDataURL(image);
-            } else {
-                createNews(content, null);
-                toast.success('News erfolgreich erstellt');
-            }
-        };
-    };
+  interface changedPost {
+    id: string
+    content: string
+    image: string | null
+  }
 
-    return (
-        <div className="flex items-center justify-center w-full">
-            <div className="w-full max-w-3xl p-8 rounded-md shadow-lg text-center">
-                <form onSubmit={handleSubmit} className="space-y-4">
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState<File | null>(null);
+
+  const [news, setNews] = useState<News[]>([])
+  const [editedPost, setEditedPost] = useState<changedPost>()
+
+  async function getNews() {
+    const allNews = await getAllNews()
+    setNews(allNews)
+  }
+
+  useEffect(() => {
+    getNews()
+  }, [])
+
+  const handleSubmit = (e: React.FormEvent) => {
+
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        createNews(content, base64String);
+        toast.success('News erfolgreich erstellt');
+
+        getNews()
+      };
+      reader.readAsDataURL(image);
+    } else {
+      createNews(content, null);
+      toast.success('News erfolgreich erstellt');
+
+      getNews()
+    }
+
+  };
+
+  const handlePostEdit = async () => {
+
+    await updateNews(editedPost!.id, editedPost!.content, editedPost!.image)
+    setEditedPost(undefined)
+
+    getNews()
+
+    toast.success('News erfolgreich bearbeitet')
+  }
+
+  const handleDeletePost = async (id: string) => {
+    await deleteNews(id)
+    getNews()
+    toast.success('News erfolgreich gelöscht')
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="w-full max-w-3xl p-8 rounded-md shadow-lg text-center">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <Textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Content"
+              required
+            />
+          </div>
+          <div>
+            <Input
+              type="file"
+              onChange={(e) => {
+                if (e.target.files) {
+                  setImage(e.target.files[0]);
+                }
+              }}
+              accept="image/*"
+            />
+          </div>
+          <Button type="submit">Posten</Button>
+        </form>
+      </div>
+
+
+      <div className="rounded-md border w-full max-w-4xl p-8 mt-16">
+        <h1 className="text-2xl font-bold mb-4 text-center">Vorherige News</h1>
+
+        <div className="bg-gray-50/2 rounded-lg shadow-lg">
+          {news.map((blog: News) => (
+            <div key={blog.id} className="shadow-lg rounded-lg bg-gray-100 p-4 mb-4">
+              <Textarea onChange={(e) => setEditedPost({ ...blog, content: e.target.value })} defaultValue={blog.content}></Textarea>
+
+              <div className="flex justify-between p-4">
+                <div>
+
+                  {editedPost?.id == blog.id && (
                     <div>
-                        <Textarea
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            placeholder="Content"
-                            required
-                        />
+                      <Button className="mr-2 bg-red-500 hover:bg-red-400" onClick={() => { setEditedPost(undefined) }}>Abbrechen</Button>
+                      <Button onClick={handlePostEdit}>Speichern</Button>
                     </div>
-                    <div>
-                        <Input
-                            type="file"
-                            onChange={(e) => {
-                                if (e.target.files) {
-                                    setImage(e.target.files[0]);
-                                }
-                            }}
-                            accept="image/*"
-                        />
-                    </div>
-                    <Button type="submit">Posten</Button>
-                </form>
+                  )}
+                </div>
+                <div>
+                  <Button className="bg-red-500 hover:bg-red-400" onClick={() => { handleDeletePost(blog.id) }}>Löschen</Button>
+                </div>
+              </div>
             </div>
-
+          ))}
         </div>
-    );
+      </div>
+
+
+    </div>
+  );
 }
